ed7ac72cf33210c1de492cf28ecac5d2cb7a6148
Andrey_Grizoglazov <greeza@yandex.ru>
2023-05-28 21:37:09 +0400

    save task

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -6,19 +6,19 @@
 Console.WriteLine("Введите любое число: ");
 string input = Console.ReadLine();
 int number = Convert.ToInt32(input);
-if (number >=10000)
+if (number >=10000 && number <=99999)
 {
     number%=1000;
-    Console.WriteLine("$ Третья цифра: {0}", number/=100);
+    Console.WriteLine("Третья цифра: {0}", number/=100);
 }
-else if (number >=1000)
+else if (number >=1000 && number <=9999)
 {
     number%=100;
-    Console.WriteLine("$ Третья цифра: {0}", number/=10);
+    Console.WriteLine("Третья цифра: {0}", number/=10);
 }
-else if (number >=100)
+else if (number >=100 && number <=999)
 {
-    Console.WriteLine("$ Третья цифра: {0}", number%=10);
+    Console.WriteLine("Третья цифра: {0}", number%=10);
 }
 else
 Console.WriteLine("Третьей цифры нет");
